d6615e0e171e6531eb757204852eae8dd6bf3dec
taysun <taysun@umich.edu>
2019-03-27 19:15:51 -0400

    Initial commit.

---
+++ odom.h
@@ -0,0 +1,20 @@
+// JerboBot Odometry Module
+// Taylor Sun (taysun@umich.edu)
+// Last updated: 3/21/19
+
+// Convert cartesian direction vector to
+// command for four omni-wheels
+
+get_pos_expec
+// based on current time, get expected position, then output result to PID
+// Define direction vector in cartesian
+
+
+
+
+// REQ: Movement command within field limits
+// MOD:	Write to omni-wheel, rotated coords
+// EFFECT:	Convert cartesian coords to rotated 45
+//			coords, relevant to omni-wheels
+void coord_convert(const int* coord_dif, int *coord_omni) { }
+
